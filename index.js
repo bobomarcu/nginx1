ab29ded2eddc0bcc1100319ac34a79698e3ec8d8
Bogdan Marcu <bmarcu@yonder.local>
2022-10-17 13:09:39 +0300

    luni amiaza

--- index.js
+++ index.js
@@ -153,12 +153,6 @@ app.post('/server-status' , (req,res)=>{
     })
 
 
-    db.query(`INSERT INTO server (id , host , port ,status) VALUES (0 , '${host}' , '${port}' , '${status}')`,(err,result)=>{
-
-        if (err) throw err
-        res.send('Complete :D\n')
-    })
-
   
 })
 
